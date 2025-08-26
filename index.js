31ae83e2334a8918799aabe5604c21572de5ce0e
404BySam <devbysam404@gmail.com>
2025-08-26 09:44:26 +0200

    insert API Jokes with fetch

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const titre = document.getElementById("title");
+
+async function getJoke() {
+  try {
+    const response = await fetch("https://v2.jokeapi.dev/joke/Any?lang=fr");
+    const data = await response.json();
+    console.log(data);
+    document.getElementById(
+      "joke"
+    ).innerText = `${data.setup} - ${data.delivery}`;
+  } catch (error) {
+    console.error("Erreur:", error);
+  }
+}
